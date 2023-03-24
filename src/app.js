b65ee97adec2b3ba9cef1bd4eeaa48de73e99a2e
Abu Bakar <bakarsafi1122@gmail.com>
2023-03-24 13:38:20 +0500

    create server

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/" , async(req,res) =>{
+    res.send("Hello from the safi")
+})
+
+app.listen(port, ()=>{
+    console.log(` connection is live at port no. ${port}`);
+})
