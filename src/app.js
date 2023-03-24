1eca4a3ace6bd74faef195b0fb87f12fe27de2c9
Abu Bakar <bakarsafi1122@gmail.com>
2023-03-24 14:52:21 +0500

    with POST data upload to DB

--- src/app.js
+++ src/app.js
@@ -4,6 +4,17 @@ const MensRanking = require ("../src/models/mens")
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+//we will handle post req
+app.post("/mens" , async (req, res)=>{
+    try{
+        const addingMensRecords = new MensRanking(req.body)
+        console.log(req.body);
+        addingMensRecords.save();
+    }catch(e){
+        res.send(e);
+    }
+})
 app.get("/" , async(req,res) =>{
     res.send("Hello from the safi")
 })
